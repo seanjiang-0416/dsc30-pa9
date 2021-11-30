800efcb65fa2a2a4094bde9a42a5d636135bdbb4
Zhixing Jiang <zhixing.jiang.sean@gmail.com>
2021-11-29 21:51:48 -0800

    Initial commit

---
+++ src/ContactLists.java
@@ -0,0 +1,119 @@
+import java.util.ArrayList;
+import java.util.*;
+
+public class ContactLists {
+
+    Hashtable<String,Person> contactList = new Hashtable<>();
+    private static final int MIDDLE_IDX = 2;
+
+    public boolean createContact(Person person){
+        if(contactList.containsKey(person.getName())){
+            return false;
+        }else {
+            contactList.put(person.getName(), person);
+            return true;
+        }
+    }
+
+    public boolean lookupContact(String name){
+        if(contactList.containsKey(name)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public Person getContact(String name){
+        return contactList.get(name);
+    }
+
+    public Person[] getContactByRange(String start, String end){
+        ArrayList<Person> temp = new ArrayList<Person>();
+        if(start.compareTo(end) >= 0){
+            throw new IllegalArgumentException();
+        }
+        Set<String> setOfKeys = this.contactList.keySet();
+
+        for (String key : setOfKeys) {
+            if (key.compareTo(start) >= 0 && key.compareTo(end) < 0) {
+                temp.add(this.contactList.get(key));
+            }
+        }
+        Person[] output = new Person[temp.size()];
+        return temp.toArray(output);
+    }
+
+    public boolean deleteContact(String name){
+        return this.contactList.remove(name) != null;
+    }
+
+    public int size(){
+        return contactList.size();
+    }
+
+    public String[] fetchAllNames(){
+
+        ArrayList<String> names =
+                new ArrayList<String>(this.contactList.keySet());
+        MergeSort(names,0,this.contactList.size()-1);
+        String [] output = new String[names.size()];
+        return names.toArray(output);
+    }
+
+    public String[] fetchAllPhoneNumbers(){
+        HashSet<String> phoneNumbers = new HashSet<>();
+        ArrayList<String> names =
+                new ArrayList<String>(this.contactList.keySet());
+        for (String name: names){
+            phoneNumbers.addAll(this.contactList.get(name).getPhoneNumbers());
+        }
+        ArrayList<String> pbList = new ArrayList<String>(phoneNumbers);
+        MergeSort(pbList,0,pbList.size()-1);
+        String[] output = new String[pbList.size()];
+        pbList.toArray(output);
+        return output;
+    }
+    private void MergeSort(ArrayList<String> list, int start, int end) {
+
+        if (start < end)
+        {
+            int mid = start + (end - start) / MIDDLE_IDX;
+            MergeSort(list, start, mid);
+            MergeSort(list , mid + 1, end);
+
+            merge(list, start, mid, end);
+        }
+    }
+
+    private void merge(ArrayList<String> arr, int l, int m, int r) {
+
+        int mergedSize = r - l + 1;
+
+        ArrayList<String> mergedNums = new ArrayList<>();
+        int left = l, right = m + 1;
+        while (left <= m && right <= r) {
+            if (arr.get(left).compareTo(arr.get(right)) <= 0) {
+                mergedNums.add(arr.get(left));
+                left++;
+            }
+            else {
+                mergedNums.add(arr.get(right));
+                right++;
+            }
+        }
+
+        while (left <= m) {
+            mergedNums.add(arr.get(left));
+            left++;
+        }
+        while (right <= r) {
+            mergedNums.add(arr.get(right));
+            right++;
+        }
+        for (int i = 0; i < mergedSize; i++) {
+            arr.set(l + i, mergedNums.get(i));
+        }
+    }
+
+}
---
+++ src/Person.java
@@ -0,0 +1,80 @@
+import java.util.ArrayList;
+
+public class Person {
+
+    String name;
+    ArrayList<String> pnArray;
+    private static final int MIDDLE_IDX = 2;
+    public Person(String name, ArrayList<String> pnArray){
+        this.name = name;
+        this.pnArray.addAll(pnArray);
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public boolean addPhoneNumber(String pn){
+        if(!this.pnArray.contains(pn)){
+            this.pnArray.add(pn);
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public ArrayList<String> getPhoneNumbers(){
+        MergeSort(this.pnArray,0,pnArray.size()-1);
+        return this.pnArray;
+    }
+    private void MergeSort(ArrayList<String> list, int start, int end) {
+
+        if (start < end)
+        {
+            int mid = start + (end - start) / MIDDLE_IDX;
+            MergeSort(list, start, mid);
+            MergeSort(list , mid + 1, end);
+
+            merge(list, start, mid, end);
+        }
+    }
+
+    private void merge(ArrayList<String> arr, int l, int m, int r) {
+
+        int mergedSize = r - l + 1;
+
+        ArrayList<String> mergedNums = new ArrayList<>();
+        int left = l, right = m + 1;
+        while (left <= m && right <= r) {
+            if (arr.get(left).compareTo(arr.get(right)) <= 0) {
+                mergedNums.add(arr.get(left));
+                left++;
+            }
+            else {
+                mergedNums.add(arr.get(right));
+                right++;
+            }
+        }
+
+        while (left <= m) {
+            mergedNums.add(arr.get(left));
+            left++;
+        }
+        while (right <= r) {
+            mergedNums.add(arr.get(right));
+            right++;
+        }
+        for (int i = 0; i < mergedSize; i++) {
+            arr.set(l + i, mergedNums.get(i));
+        }
+    }
+
+    public boolean deletePhoneNumber(String pn){
+        if(this.pnArray.size() == 1){
+            throw new IllegalArgumentException();
+        }
+        return this.pnArray.remove(pn);
+    }
+}
+
